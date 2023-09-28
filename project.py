7a3393eb3a32f67706afd65595906d42eb30d336
mkerley07 <michaelkerley07@gmail.com>
2023-09-28 16:40:56 -0400

    modified:   project.py

--- project.py
+++ project.py
@@ -1,3 +1,5 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 import calendar
 import datetime
 import smtplib
@@ -32,7 +34,36 @@ def send_email(receiver_email, subject, message):
         print(f"An Error Occurred: {str(e)}")
 
 
-# Function to send assignment notifications
+# Define the scope and credentials file path
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+credentials = ServiceAccountCredentials.from_json_keyfile_name("C:/Users/Mkerl/Downloads/calendernotif-400322-015ca3a312f6.json", scope)
+
+# Authenticate using the credentials
+gc = gspread.authorize(credentials)
+    
+# Open the Google Sheets document by name
+spreadsheet = gc.open_by_url("https://docs.google.com/spreadsheets/d/1dwAd7sNk4RVsPYALsMgyP0-euXYB_parh5YPml6Zj-I/edit#gid=0")
+
+
+# Select the specific worksheet (page) by title
+worksheet = spreadsheet.worksheet("Sheet1")
+
+# Fetch data from the worksheet as a list of dictionaries, skipping the first row (header row)
+data = worksheet.get_all_records(head=1)  # Specify 'head=2' to skip the first row
+
+# Process and send notifications
+assignments = []
+for row in data:
+    year = int(row['year'])
+    month = int(row['month'])
+    day = int(row['day'])
+    assignment_type = row['assignment_type']
+    assignment_description = row['assignment_description']
+
+    assignments.append((year, month, day, assignment_type, assignment_description))  # Append each assignment to the list
+
+
+
 def send_assignment_notif(assignments, receiver_email):
     # Assignment Type Dictionary
     assignment_types = {
@@ -52,40 +83,44 @@ def send_assignment_notif(assignments, receiver_email):
 
         # Check if the event is in the future or due today
         event_date = datetime.date(year, month, day)
+        today = datetime.date.today()
 
         if assignment_type == 'exam':
             exam_notif_date = event_date - datetime.timedelta(days=5)
-            if datetime.date.today() < event_date and datetime.date.today() >= exam_notif_date:
-                
+            if today <= event_date and today >= exam_notif_date:
                 send_email(receiver_email, "Exam Notification", event_description)
-            elif event_date == datetime.date.today():
-                    send_email(receiver_email, "EXAM TODAY", event_description)
+            elif today == event_date:
+                send_email(receiver_email, "EXAM TODAY", event_description)
         
         elif assignment_type == 'quiz':
             quiz_notif_date = event_date - datetime.timedelta(days=3)
-            if datetime.date.today() < event_date and datetime.date.today() >= quiz_notif_date:
-                
+            if today <= event_date and today >= quiz_notif_date:
                 send_email(receiver_email, "Quiz Notification", event_description)
-            elif event_date == datetime.date.today():
-                    send_email(receiver_email, "QUIZ TODAY", event_description)
+            elif today == event_date:
+                send_email(receiver_email, "QUIZ TODAY", event_description)
         
         elif assignment_type == 'homework':
             hw_notif_date = event_date - datetime.timedelta(days=2)
-            if datetime.date.today() < event_date and datetime.date.today() >= hw_notif_date:
-                
+            if today <= event_date and today >= hw_notif_date:
                 send_email(receiver_email, "Homework Notification", event_description)
-            elif event_date == datetime.date.today():
-                    send_email(receiver_email, "HOMEWORK DUE TODAY", event_description)
+            elif today == event_date:
+                send_email(receiver_email, "HOMEWORK DUE TODAY", event_description)
+    print(event_description)
+
+
 
        
 
 # List of Assignments
-assignments = [
-    (2023, 9, 27, 'exam', 'Midterm Exam'),
-    (2023, 9, 27, 'quiz', 'Quiz 1'),
-    (2023, 9, 28, 'homework', 'Homework 3'),
-    (2023, 8, 13, 'homework', 'Homework 17'),
-]
+#assignments = [
+ #   (2023, 9, 2, 'exam', 'Midterm Exam'),
+  #  (2023, 9, 27, 'quiz', 'Quiz 1'),
+   # (2023, 9, 28, 'homework', 'Homework 3'),
+    #(2023, 8, 13, 'homework', 'Homework 17'),
+#]
+
+
+
 
 receiver_email = 'michaelkerley07@gmail.com'
 
